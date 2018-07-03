536fb6c895fa0aa7ae891e397ab2cb912bb407c2
saberry <seth.berry@nd.edu>
2018-07-03 11:19:32 -0400

    tract getter

---
+++ code/tractGetter.R
@@ -0,0 +1,54 @@
+library(rgdal)
+
+sjcShape = sf::read_sf("data/parcelShape/parcel2017.shp")
+
+sjcShape %>% 
+  select(MAILINGADD, MAILINGCIT, MAILINGSTA, MAILINGZIP) %>% 
+  write.csv(., "addresses.csv", row.names = FALSE, col.names = FALSE)
+
+tractGetter = function(street, city, zip){
+  
+  Sys.sleep(.5)
+  
+  test = httr::GET(paste("https://geocoding.geo.census.gov/geocoder/geographies/address?street=", 
+                         gsub("\\s", "\\+", street), "&city=", city, 
+                         "&state=IN&zip=", zip, "&benchmark=9&vintage=910&format=json", 
+                         sep = ""))
+  out = tryCatch({
+    tract = content(test)$result$addressMatches[[1]]$geographies$`Census Tracts`[[1]]$BASENAME
+    
+    return(tract)
+  }, 
+  error = function(e) {
+    return(NA)
+  })
+  
+  return(out)
+}
+
+allTracts = mapply(tractGetter, sjcShape$MAILINGADD, sjcShape$MAILINGCIT, sjcShape$MAILINGZIP, 
+                   SIMPLIFY = FALSE)
+
+tractGetter(sjcShape$MAILINGADD[1000], sjcShape$MAILINGCIT[1000], sjcShape$MAILINGZIP[1000])
+
+tract = content(test)$result$addressMatches[[1]]$geographies$`Census Blocks`[[1]]$TRACT
+
+inShape = rgdal::readOGR("data/tl_2017_18_tract/tl_2017_18_tract.shp", 
+                         layer = "tl_2017_18_tract")
+
+inShape = inShape[inShape$COUNTYFP == 141,]
+
+library(leaflet)
+
+leaflet() %>% 
+  addPolylines(data = inShape) %>% 
+  addCircles(data = sjcShape, lng = ~INSIDE_X_D, lat = ~Y_Degrees)
+
+library(tidycensus)
+
+
+
+census_api_key("99f02d15745b04acaf40f60535ae45a70ecde2ac")
+
+tidycensus::get_decennial(geography = "tract", variables = "P0050003",
+                          state = "IN", county = "141", year = 2010)
